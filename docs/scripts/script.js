ed2001a92f27389738770c45a361158d566a87aa
Leonardo William Lopes <leonardolopessocial@gmail.com>
2021-09-13 21:10:09 -0300

    Set13 - 21:10

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -2,3 +2,7 @@ window.addEventListener('scroll', function(){
     let header = document.querySelector('.scroll')
     header.classList.toggle('anime', window.scrollY > 0);
 })
+function copy_email(){
+    navigator.clipboard.writeText("Leonardolopessocial@gmail.com")
+    alert('Copiado para Clipboard')
+}
