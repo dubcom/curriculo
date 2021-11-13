bbc5d9c93f604a378bff5b8e87f92af98799cef6
Leonardo William Lopes <62454440+LeonardoWlopes@users.noreply.github.com>
2021-11-12 22:56:32 -0300

    Nov12 -  22:55

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="TopContainer">
+      <header>
+        <nav className="NavBar">
+          <a href="#">Sobre</a>
+          <a href="#">Skills</a>
+          <a href="#">Laboratorio</a>
+          <a href="#">Contato</a>
+          
+        </nav>
+      </header>
+      <main>
+        <div className='teste'>Teste</div>
+      </main>
+    </div>
+  );
+}
