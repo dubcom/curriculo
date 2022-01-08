46c90db16ff38346000c07643218b1442900d26c
Leonardo William Lopes <62454440+LeonardoWlopes@users.noreply.github.com>
2022-01-08 17:32:05 -0300

    Jan08 - 17:30

--- src/Screens/Main/index.tsx
+++ src/Screens/Main/index.tsx
@@ -5,7 +5,7 @@ import UserCard from "../../Components/Cards/UserCard";
 import ContactCard from "../../Components/Cards/ContactCard";
 import SkillCard from "../../Components/Cards/SkillCard";
 import Repos from "../../Components/Repos";
-import GitHubStats from "../../Components/GithubStats";
+import GitHubStats from "../../Components/Repos copy";
 
 export default function MainScreen() {
   const [isColumn, setIsColumn] = useState(false);
