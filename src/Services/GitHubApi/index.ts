de9938e17c0f1852acf39d351f33fa05f0401f4a
Marcos Antonio <58779341+dubcom@users.noreply.github.com>
2022-08-09 06:49:32 -0300

    new user

--- src/Services/GitHubApi/index.ts
+++ src/Services/GitHubApi/index.ts
@@ -2,7 +2,7 @@ import { IUser } from "./../../Interfaces/User.interface";
 import axios from "axios";
 
 const GitHubApi = (): Promise<IUser> =>
-  axios.get("https://api.github.com/users/leonardowlopes").then((res) => {
+  axios.get("https://api.github.com/users/dubcom").then((res) => {
     console.log(res.data);
     return res.data;
   });
